1633c647c465d2f595e1015b753e58792da26a3f
EKBJ <EKBJ@novonordisk.com>
2024-02-29 08:52:16 +0100

    Fixed margins to be consistent by trimming dimensions

--- main.py
+++ main.py
@@ -10,15 +10,22 @@ This project uses svg.py to generate the svg code.
 """
 
 import svg
+import fpdf2
+import math
 
-MARGIN = 20
-GAP = 20
-WIDTH = 200
-HEIGHT = 500
+STRICT = True  # Trim board to preserve standard margin or not
+MARGIN = 20  # Default 20
+GAP = 20  # Default 20
+WIDTH = 230  # Multiple of 20
+HEIGHT = 500  # Multiple of 20
 STROKE = 0.02
 
-SHIFT = MARGIN
-STEP = GAP
+SHIFT = 20  # Default 20
+
+# Trim the sheet dimensions to multiple of 20
+if STRICT:
+    WIDTH = math.floor(WIDTH / 20) * 20
+    HEIGHT = math.floor(HEIGHT / 20) * 20
 
 
 def draw() -> svg.SVG:
@@ -41,12 +48,12 @@ def draw() -> svg.SVG:
 
     # Draw Pill-shapes
     do_shift = True
-    for y in range(MARGIN, HEIGHT, STEP):
+    for y in range(MARGIN, HEIGHT - MARGIN + 1, GAP):
         do_shift = not do_shift
         start_x = MARGIN
         if do_shift:
             start_x += SHIFT
-        for x in range(start_x, WIDTH, STEP * 2):
+        for x in range(start_x, WIDTH - MARGIN + 1, GAP * 2):
             elements.append(
                 svg.Path(d=[
                     svg.M(x - 2.5, y - 5),
