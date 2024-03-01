dc1cf79017eacce94d968031d6a46f83e13c5a97
EKBJ <EKBJ@novonordisk.com>
2024-03-01 13:08:51 +0100

    change draw function to have inputs and not rely on constants

--- svg_generator.py
+++ svg_generator.py
@@ -13,13 +13,13 @@ import svg
 import fpdf
 
 STRICT = True  # Trim board to preserve standard margin or not
-X_MARGIN = 20  # Default 20
-Y_MARGIN = 20  # Default 20
-GAP = 20  # Default 20
+x_margin = 20  # Default 20
+y_margin = 20  # Default 20
+gap = 20  # Default 20
 WIDTH = 200  # Multiple of 20
 HEIGHT = 500  # Multiple of 20
-STROKE = 0.02
-LANDSCAPE = True
+stroke = 0.02
+landscape = True
 
 SHIFT = 20  # Default 20
 
@@ -29,11 +29,24 @@ if STRICT:
     HEIGHT = (HEIGHT // 20) * 20
 
 
-def draw() -> svg.SVG:
+def draw(width,
+         height,
+         x_margin=20,
+         y_margin=20,
+         landscape=False,
+         trim=True,
+         gap=20,
+         stroke=0.02) -> svg.SVG:
     """
     Draws the Skådis sheet
     """
     elements: list[svg.Element] = []
+    SHIFT = 20
+
+    # Trim the sheet dimensions to multiple of 20
+    if trim:
+        width = (width // 20) * 20
+        height = (height // 20) * 20
 
     # Draw bounding box
     elements.append(
@@ -41,20 +54,20 @@ def draw() -> svg.SVG:
                  y=0,
                  rx=8,
                  ry=8,
-                 width=WIDTH,
-                 height=HEIGHT,
-                 stroke_width=STROKE,
+                 width=width,
+                 height=height,
+                 stroke_width=stroke,
                  fill="pink",
                  stroke="black"))
 
     # Draw Pill-shapes
     do_shift = True
-    for y in range(Y_MARGIN, HEIGHT - Y_MARGIN + 1, GAP):
+    for y in range(y_margin, height - y_margin + 1, gap):
         do_shift = not do_shift
-        start_x = X_MARGIN
+        start_x = x_margin
         if do_shift:
             start_x += SHIFT
-        for x in range(start_x, WIDTH - X_MARGIN + 1, GAP * 2):
+        for x in range(start_x, width - x_margin + 1, gap * 2):
             elements.append(
                 svg.Path(d=[
                     svg.M(x - 2.5, y - 5),
@@ -63,33 +76,34 @@ def draw() -> svg.SVG:
                     svg.a(2.5, 2.5, 0, 0, 1, -5, 0),
                     svg.Z()
                 ],
-                         stroke_width=STROKE,
+                         stroke_width=stroke,
                          stroke="black",
                          fill="white"))
 
-    if LANDSCAPE:
+    if landscape:
         group = svg.G(elements=elements,
                       transform=[
-                          svg.Translate(0, WIDTH),
+                          svg.Translate(0, width),
                           svg.Rotate(-90),
                       ])
-        W = HEIGHT
-        H = WIDTH
+        width, height = height, width
     else:
         group = svg.G(elements=elements)
-        W = WIDTH
-        H = HEIGHT
 
     return svg.SVG(
-        viewBox=svg.ViewBoxSpec(0, 0, W, H),
-        width=str(W) + "mm",
-        height=str(H) + "mm",
+        viewBox=svg.ViewBoxSpec(0, 0, width, height),
+        width=str(width) + "mm",
+        height=str(height) + "mm",
         elements=[group],
     )
 
 
 if __name__ == '__main__':
-    canvas = draw()
+
+    WIDTH = 200
+    HEIGHT = 380
+
+    canvas = draw(WIDTH, HEIGHT)
     with open('output.svg', 'w', encoding='UTF-8') as file:
         file.write(str(canvas))
 
@@ -99,6 +113,5 @@ if __name__ == '__main__':
     pdf.add_page()
     svg_object.draw_to_page(pdf)
 
-    pdf_filename = f"IKEA_Skådis_{WIDTH}X{HEIGHT}_{'Landscape' if LANDSCAPE else 'Portrait'}.pdf"
-
+    pdf_filename = f"IKEA_Skådis_{WIDTH}X{HEIGHT}_{'Landscape' if landscape else 'Portrait'}.pdf"
     pdf.output(pdf_filename)
